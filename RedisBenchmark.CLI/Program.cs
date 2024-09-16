f1c71bcc5e87cf347cc758d164c2e6ab28dff37a
Михаил Сафронов <muxaul@git.imbalanced.tech>
2024-09-17 00:47:03 +0300

    first work

--- RedisBenchmark.CLI/Program.cs
+++ RedisBenchmark.CLI/Program.cs
@@ -1,24 +1,32 @@
 ﻿using RedisBenchmark.CLI.Parts;
 using StackExchange.Redis;
-using System.Reactive.Linq;
+using Microsoft.Extensions.Configuration;
 
-const string connectionString = "redis-server:6379";
+var config = new ConfigurationBuilder()
+  .SetBasePath(Directory.GetCurrentDirectory()) 
+  .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true) 
+  .Build();
+
+var connectionString = config["ConnectionString"];
 const string channel = "benchmark_channel";
 
-var redis = await ConnectionMultiplexer.ConnectAsync(connectionString);
-
-var publisher = new Publisher(redis, channel);
-
-var subscriber1 = new Subscriber(redis, channel, "Subscriber1");
-var subscriber2 = new Subscriber(redis, channel, "Subscriber2");
-var subscriber3 = new Subscriber(redis, channel, "Subscriber3");
-var subscriber4 = new Subscriber(redis, channel, "Subscriber4");
-var subscriber5 = new Subscriber(redis, channel, "Subscriber5");
-
-for (var i = 1; i <= 5; i++)
+if (connectionString != null)
 {
-  await publisher.PublishMessagesAsync($"Сообщение {i}");
-  await Task.Delay(5000);
+  var redis = await ConnectionMultiplexer.ConnectAsync(connectionString);
+
+  var publisher = new Publisher(redis, channel);
+
+  var subscriber1 = new Subscriber(redis, channel, "Subscriber1");
+  var subscriber2 = new Subscriber(redis, channel, "Subscriber2");
+  var subscriber3 = new Subscriber(redis, channel, "Subscriber3");
+  var subscriber4 = new Subscriber(redis, channel, "Subscriber4");
+  var subscriber5 = new Subscriber(redis, channel, "Subscriber5");
+
+  for (var i = 1; i <= 5; i++)
+  {
+    await publisher.PublishMessagesAsync($"Сообщение {i}");
+    await Task.Delay(5000);
+  }
 }
 
 Console.WriteLine("Нажмите любую клавишу для завершения...");
